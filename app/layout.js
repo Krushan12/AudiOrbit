0becabd85f4f20e13488f266989241adeab80986
Krushant <krushantw@gmail.com>
2025-06-07 21:07:58 +0530

    feat: Replace favicon with updated SVG version and add favicon link in layout

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      </head>
       <body suppressHydrationWarning className="min-h-screen bg-gradient-to-b from-gray-900 to-black text-white">
         <FirebaseProvider>
           {children}
